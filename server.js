01f0e9682686a12aad847d0d866374eabe5d6989
jeffreyjv <jeffjv@live.unc.edu>
2022-02-28 15:50:11 -0500

    tessssst

--- server.js
+++ server.js
@@ -1,60 +1,77 @@
-import * as coin from "./modules/coin.mjs";
-import minimist from "minimist";
-import express from "express";
 
+   
+import minimist from 'minimist'
+import express from 'express'
 
-const args = minimist(process.argv.slice(2));
-args['port'];
+var args = minimist(process.argv.slice(2))
 
-const app = express();
-const port = args.port || process.env.PORT || 5000;
-
+const app = express()
+const port = args.port || process.env.PORT || 5000
 
 const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%', port));
+    console.log('App is running on port %PORT%'.replace('%PORT%', port))
 })
 
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+  }
+
+function coinFlips(flips) {
+    let f = [];
+    for (var i = 0; i < flips; i++) {
+      let flip = coinFlip();
+      f[i] = flip;
+    }
+    return f;
+  }
+
+function countFlips(array) {
+    let heads_count = 0;
+    let tails_count = 0;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] == 'heads') {
+        heads_count ++;
+      } else if (array[i] == 'tails') {
+        tails_count ++;
+      }
+    }
+    if (heads_count == 0) {
+      return {"tails": tails_count};
+    }
+    else if (tails_count == 0) {
+      return {"heads": heads_count};
+    }
+    return {"heads": heads_count, "tails": tails_count};
+  }
+  
+function flipACoin(call) {
+    let coin_flip = coinFlip();
+    let match = "";
+    if (coin_flip == call) {
+      match = "win";
+    } else if (coin_flip != call) {
+      match = "lose";
+    }
+    return {"call": call, "flip": coin_flip, "result": match};
+  }
 
 app.get('/app/', (req, res) => {
-    res.statusCode = 200; 
-    res.statusMessage = 'OK';
-    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
-    res.end(res.statusCode + ' ' + res.statusMessage);
-});
-
-
+    res.status(200).end("OK")
+    res.type('text/plain')
+})
 app.get('/app/flip/', (req, res) => {
-    let flip = coin.coinFlip();
-    res.status(200).json({ "flip": flip });
-});
-
-app.get('/app/flips/:number', (req, res) => {
-    let raw = coin.coinFlips(req.params.number);
-    let summary = coin.countFlips(raw);
-    res.status(200).json({ "raw": raw, "summary": summary });
-});
-
-
-app.get('/app/flip/call/heads', (req, res) => {
-    let heads = coin.flipACoin('heads');
-    let call = heads.call;
-    let flip = heads.flip;
-    let result = heads.result;
-    res.status(200).json({ "call": call, "flip": flip, "result": result });
-});
-
-
-app.get('/app/flip/call/tails', (req, res) => {
-    let tails = coin.flipACoin('tails');
-    let call = tails.call;
-    let flip = tails.flip;
-    let result = tails.result;
-    res.status(200).json({ "call": call, "flip": flip, "result": result });
-});
-
-
-
-app.use(function (req, res) {
-    res.status(404).send('404 NOT FOUND');
-    res.type("text/plain");
-});
+    res.status(200).json({'flip': coinFlip()})
+})
+app.get('/app/flips/:number/', (req, res) => {
+    res.status(200).json({'raw': coinFlips(req.params.number), 'summary': countFlips(coinFlips(req.params.number))})
+})
+app.get('/app/flip/call/tails/', (req, res) => {
+    res.status(200).json(flipACoin('tails'))
+})
+app.get('/app/flip/call/heads/', (req, res) => {
+    res.status(200).json(flipACoin('heads'))
+})
+app.use(function(req, res) {
+    res.status(404).send("Endpoint does not exist")
+    res.type("text/plain")
+})
